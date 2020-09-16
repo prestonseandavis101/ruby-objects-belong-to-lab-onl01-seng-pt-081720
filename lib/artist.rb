03cc2d70e2b0d40cf6cb7d9dc6eb015f53efc98d
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-16 00:31:49 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist 
+  attr_accessor :name,
+  def initialize(name)
+    @name= name
+end
+end
+
+
+class Author
+  attr_accessor :name
+  end
+  
+  class Post
+    attr_accessor :title
+  end
+
